5c0eb27d6a9bc45df64a3868a9f440b78f038203
thelowlypeon <me@petercompernolle.com>
2025-06-08 20:20:24 -0500

    fixes EventMessage enum fields (temporarily integer fields)

--- Sources/PureFIT/FITProfile/Messages/EventMessage.swift
+++ Sources/PureFIT/FITProfile/Messages/EventMessage.swift
@@ -12,12 +12,15 @@ public struct EventMessage: ProfiledMessage {
     public enum Field: UInt8, CaseIterable, StandardMessageField, FieldDefinitionProviding {
         case event = 0
         case eventType = 1
+        // case data = 3 // this is a UInt32 with lots of possible value types
+        case eventGroup = 4
         case timestamp = 253
 
         public var fieldDefinition: any FieldDefinition {
             switch self {
-            case .event: IntegerField(name: "Event", baseType: .enum) // TODO: Enum field
-            case .eventType: IntegerField(name: "Event Type", baseType: .enum) // TODO: Enum field
+            case .event: EnumField<Event>(name: "Event", baseType: .enum, enumType: .activity)
+            case .eventType: EnumField<EventType>(name: "Event Type", baseType: .enum, enumType: .start)
+            case .eventGroup: IntegerField(name: "Event Type", baseType: .uint8)
             case .timestamp: DateField(name: "Timestamp")
             }
         }
