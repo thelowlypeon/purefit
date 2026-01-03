68ec394cde2b5b4df2304983c1840a2e1df469c7
thelowlypeon <me@petercompernolle.com>
2026-01-03 06:35:49 -0600

    adds garmin product enum for FileID message

--- Sources/PureFIT/FITProfile/Messages/FileIDMessage.swift
+++ Sources/PureFIT/FITProfile/Messages/FileIDMessage.swift
@@ -12,7 +12,7 @@ public struct FileIDMessage: ProfiledMessage {
     public enum Field: UInt8, CaseIterable, StandardMessageField, FieldDefinitionProviding {
         case type = 0
         case manufacturer = 1
-        // case product = 2 // two values
+        case product = 2
         case serialNumber = 3
         case timeCreated = 4
         case productName = 8
@@ -20,6 +20,7 @@ public struct FileIDMessage: ProfiledMessage {
         public var fieldDefinition: any FieldDefinition {
             switch self {
             case .type: return EnumField<File>(name: "Type", baseType: .enum, enumType: .activity)
+            case .product: return EnumField<GarminProduct>(name: "Product", baseType: .enum, enumType: .hrm1)
             case .manufacturer: return EnumField<Manufacturer>(name: "Manufacturer", baseType: .uint16, enumType: .garmin)
             case .serialNumber: return IntegerField(name: "Serial Number", baseType: .uint32z)
             case .timeCreated: return DateField(name: "Time Created")
