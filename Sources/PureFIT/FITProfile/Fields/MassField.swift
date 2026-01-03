69a1e5d1362ecb993bae8fdc6516efe6a6bf4eed
thelowlypeon <me@petercompernolle.com>
2026-01-03 15:07:17 -0600

    adds MassField

---
+++ Sources/PureFIT/FITProfile/Fields/MassField.swift
@@ -0,0 +1,25 @@
+//
+//  DistanceField.swift
+//  PureFIT
+//
+//  Created by Peter Compernolle on 3/26/25.
+//
+
+import Foundation
+
+public struct MassField: NamedFieldDefinition, DimensionalFieldDefinition, MeasurableFieldDefinition {
+    public struct Value: FieldValue, MeasurementValue {
+        public let measurement: Measurement<UnitMass>
+    }
+
+    public let name: String
+    public let baseType: FITBaseType
+    public let unit: UnitMass
+    public let scale: Double
+    public let offset: Double
+
+    public func parse(values: [FITValue]) -> Value? {
+        guard let value = values.first, let measurement = measurement(value) else { return nil }
+        return Value(measurement: measurement)
+    }
+}
